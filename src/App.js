21a54e4b21e78f7730679e28f7a5c7f644dcf42f
JuanIgnacioMarchetto <tu@email.com>
2024-02-28 08:37:34 -0300

    28/2/2024-08:37

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+<div className='Maincontainer'>
+    <div className='Mainhead'>
+        <h1>JG & Asociados</h1>
+        <nav>
+            <ul>
+                <li><a href="#inicio">INICIO</a></li>
+                <li><a href="#quienes-somos">QUIÉNES SOMOS</a></li>
+                <li><a href="#areas-practica">ÁREAS DE PRÁCTICA</a></li>
+                <li><a href="#contacto">CONTÁCTENOS</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <main>
+        <section id="inicio">
+            <h2>INICIO</h2>
+           {/*  -- Contenido de la sección de inicio  */}
+        </section>
+
+        <section id="quienes-somos">
+            <h2>QUIÉNES SOMOS</h2>
+            <p>JG & Asociados es una firma especializada en la asesoría y servicios de representación en materia legal y jurídica. Somos un estudio comprometido profesional y humanamente con el servicio satisfactorio a los clientes. Nos enfocamos en asesorar jurídicamente a empresas y particulares. Contamos con profesionales especializados en cada una de las áreas. Damos pronta solución a sus requerimientos. Realizamos seguimiento personalizado a cada caso. Ofrecemos información al cliente para que conozca los pasos de su situación.</p>
+        </section>
+
+        <section id="areas-practica">
+            <h2>ÁREAS DE PRÁCTICA</h2>
+           {/*  <!-- Contenido de la sección de áreas de práctica --> */}
+        </section>
+
+        <section id="contacto">
+            <h2>CONTÁCTENOS</h2>{/* 
+            <!-- Contenido de la sección de contacto --> */}
+        </section>
+    </main>
+
+    <footer>
+        <p>Desarrollado por Ingplat | JG & Asociados Estudio Jurídico</p>
+    </footer>
+
+ </div> 
+
       </header>
     </div>
   );
